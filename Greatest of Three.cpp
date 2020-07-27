3e93b61569528975a9d4ba42628cad8921572d3a
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2020-07-27 17:14:19 +0530

    Second commit

--- Greatest of Three.cpp
+++ Greatest of Three.cpp
@@ -2,10 +2,11 @@
 
 int main(){
 
-    int a=0,b=0,c=0;
+    int a=0, b=0, c=0;
     int largest=0;
     std::cout<<"Input three numbers: ";
     std::cin>>a>>b>>c;
+
     if(a>b&&a>c){
         largest=a;
     }
